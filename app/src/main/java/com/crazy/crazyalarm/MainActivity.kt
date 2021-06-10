020f4b0fc273d08add301febebf5adf3fd5c1363
Chains.Z <10195101497@stu.ecnu.edu.cn>
2021-06-10 23:35:01 +0800

    继续写mainActivity

--- app/src/main/java/com/crazy/crazyalarm/MainActivity.kt
+++ app/src/main/java/com/crazy/crazyalarm/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.crazy.crazyalarm.clockUtils.AlarmManagerUtil
 import com.crazy.crazyalarm.databinding.ActivityMainBinding
+import java.time.Clock
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var list = mutableListOf<Clock>()
+
         val calendar = Calendar.getInstance()
-        binding.button2.setOnClickListener {
+//        binding.button2.setOnClickListener {
 //            val intent = AlarmManagerUtil.setAlarm(
 //                this,
 //                calendar.get(Calendar.HOUR_OF_DAY),
@@ -28,8 +31,8 @@ class MainActivity : AppCompatActivity() {
 //                AlarmManagerUtil.Norm
 //            )
 //            Toast.makeText(this, "闹钟设置成功", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, SetClockActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this, SetClockActivity::class.java)
+//            startActivity(intent)
 //            val intent = Intent(AlarmManagerUtil.ALARM_ACTION)
 //            intent.putExtra(AlarmManagerUtil.MSG, "闹钟响了")
 //            intent.putExtra(AlarmManagerUtil.INTERVALLMILLIS, "0")
@@ -38,4 +41,3 @@ class MainActivity : AppCompatActivity() {
 //            sendBroadcast(intent)
         }
     }
-}
---
+++ app/src/main/java/com/crazy/crazyalarm/clockUtils/ClockItem.kt
@@ -0,0 +1,4 @@
+package com.crazy.crazyalarm.clockUtils
+
+class ClockItem {
+}
